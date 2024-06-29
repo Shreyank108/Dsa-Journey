abb8811121a7403c3d7a9d9ad5f5c681a5339a26
shreyank108 <shreyankagrawal17@gmail.com>
2024-06-29 20:20:14 +0530

    opps stage 1

---
+++ OOPm/Acess specifier .cpp
@@ -0,0 +1,25 @@
+//  3 specifiers   
+//    private   -> itself, friend    
+//    protected  -> itself , inherited   
+//    public   ->  anyone can   
+
+// using namespace std ;
+// #include <iostream>
+// class A{ 
+//     private: 
+//      int a   ;
+//     protected: 
+//       int b;  
+//     public: 
+//      int c =100; 
+// } ;
+// int main(){ 
+//     A obj ; 
+//     // obj.a=10 ; 
+//     // obj.b=20; 
+
+//     // cout<<obj.a<<endl;
+//     // cout<<obj.b<<endl;
+//     cout<<obj.c<<endl;
+// } 
+
---
+++ OOPm/first .cpp
@@ -0,0 +1,14 @@
+using namespace std ;
+#include <iostream>  
+class Print{  
+    public:
+    void shoe(){ 
+            cout << "Hii i am shreyank" ;
+    }
+    
+};
+int main(){ 
+   Print obj ; 
+   obj.shoe(); 
+   return 0; 
+}
