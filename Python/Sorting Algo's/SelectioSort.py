db602b0c7ce20394d07d9f3cd7854f983ebdd8a4
Shreyank Agrawal <122221017+Shreyank108@users.noreply.github.com>
2024-08-21 23:55:51 +0530

    searching and sorting completed

--- Python/Sorting Algo's/SelectioSort.py
+++ Python/Sorting Algo's/SelectioSort.py
@@ -8,3 +8,19 @@
 
 s="a4c5d6" 
 output= "aaaacccccdddddd" 
+i=0 
+result = "" 
+while i <len(s): 
+    char= s[i] 
+    i+=1  
+    string_num =""
+    while i<len(s) and s[i].isdigit() : 
+        string_num+=s[i] 
+        i+=1   
+    converting_to_num= int(string_num)  
+    result+=char*converting_to_num   
+print(result) 
+if result == output: 
+    print("Result Matches") 
+else : 
+    print("Not Matched")
